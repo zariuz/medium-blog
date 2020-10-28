e428c346553f52262c4bdce85f22dd95be9d622c
unknown <nechetov@hotmail.com>
2020-10-28 13:31:32 +0300

    add global feed

--- src/pages/GlobalFeed/index.jsx
+++ src/pages/GlobalFeed/index.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { Feed } from 'components/Feed';
+import { useFetch } from 'hooks/useFetch';
 
 export const GlobalFeed = () => {
-  return <div>GlobalFeed</div>;
+  const apiUrl = '/articles?limit=10&offset=0';
+  const [{ response, error, isLoading }, doFetch] = useFetch(apiUrl);
+
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <h1>Medium Clone</h1>
+        <p>A place to share knowledge</p>
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            {isLoading && <div>Loading...</div>}
+            {error && <div>Some error happened</div>}
+            {!isLoading && response && (
+              <>
+                <Feed articles={response.articles} />
+              </>
+            )}
+          </div>
+          <div className="col-md-3">Popular tags</div>
+        </div>
+      </div>
+    </div>
+  );
 };
